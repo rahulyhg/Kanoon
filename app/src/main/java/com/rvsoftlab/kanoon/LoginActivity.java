f5f2b2daaa12771aa906293af0daa7a92bc364b8
RVishwakarma <ravikant.vishwakarma@technogen.net>
2017-12-22 17:55:14 +0530

    Firebase Custom Auth Added
    Signed-off-by: RVishwakarma <ravikant.vishwakarma@technogen.net>

--- app/src/main/java/com/rvsoftlab/kanoon/LoginActivity.java
+++ app/src/main/java/com/rvsoftlab/kanoon/LoginActivity.java
@@ -1,31 +1,81 @@
 package com.rvsoftlab.kanoon;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.lang.reflect.Method;
+
 public class LoginActivity extends AppCompatActivity {
 
     Button btnLogin;
+    private FirebaseAuth mAuth;
+    private RequestQueue requestQueue;
+    private String TAG = LoginActivity.class.getSimpleName();
+    private FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         if (getSupportActionBar()!=null){
             getSupportActionBar().setTitle("");
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
         btnLogin = findViewById(R.id.btn_login);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                authenticate();
+            }
+        });
+        mAuth = FirebaseAuth.getInstance();
+        requestQueue = Volley.newRequestQueue(this);
+    }
 
+    private void authenticate() {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, "http://rvsoft.esy.es/Android/kanoon/firebase.php", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG,response);
+                signInWithFirebase(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(stringRequest);
+    }
+
+    private void signInWithFirebase(String token) {
+        mAuth.signInWithCustomToken(token).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    user = task.getResult().getUser();
+                    Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                }else {
+                    task.getException().printStackTrace();
+                }
             }
         });
     }
@@ -56,5 +106,16 @@ public class LoginActivity extends AppCompatActivity {
             Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        user = mAuth.getCurrentUser();
+        if (user==null){
+            Toast.makeText(this, "Not logged in", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Logged in", Toast.LENGTH_SHORT).show();
+        }
     }
 }
