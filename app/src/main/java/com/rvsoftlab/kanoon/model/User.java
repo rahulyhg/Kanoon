e299098ce60689335a480c8211a9cf66c7253193
RVishwakarma <ravikant.vishwakarma@technogen.net>
2017-12-26 17:41:12 +0530

    Initial Commit
    Signed-off-by: RVishwakarma <ravikant.vishwakarma@technogen.net>

---
+++ app/src/main/java/com/rvsoftlab/kanoon/model/User.java
@@ -0,0 +1,11 @@
+package com.rvsoftlab.kanoon.model;
+
+/**
+ * Created by RVishwakarma on 12/26/2017.
+ */
+
+public class User {
+    private String name;
+    private String mobile;
+    //private String
+}
